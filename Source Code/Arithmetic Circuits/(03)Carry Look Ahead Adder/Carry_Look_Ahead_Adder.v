4af20806811dd091292f3e65083aa270103ad714
Mohitdesai27 <mohit.s.desai2705@gmail.com>
2026-04-06 19:13:56 +0530

    Update Carry_Look_Ahead_Adder.v

--- Source Code/Arithmetic Circuits/(03)Carry Look Ahead Adder/Carry_Look_Ahead_Adder.v
+++ Source Code/Arithmetic Circuits/(03)Carry Look Ahead Adder/Carry_Look_Ahead_Adder.v
@@ -1,9 +1,19 @@
-module ripple_carry_adder(input [3:0] A, B, input Cin,
-                          output [3:0] SUM, output Cout);
+module cla_4bit(
+  input [3:0] A, B, 
+  input Cin,
+  output [3:0] SUM, 
+  output Cout
+);
+    wire [3:0] P, G;
     wire c1, c2, c3;
 
-    full_adder FA0(A[0], B[0], Cin, SUM[0], c1);
-    full_adder FA1(A[1], B[1], c1, SUM[1], c2);
-    full_adder FA2(A[2], B[2], c2, SUM[2], c3);
-    full_adder FA3(A[3], B[3], c3, SUM[3], Cout);
+    assign P = A ^ B;
+    assign G = A & B;
+
+    assign c1 = G[0] | (P[0] & Cin);
+    assign c2 = G[1] | (P[1] & c1);
+    assign c3 = G[2] | (P[2] & c2);
+    assign Cout = G[3] | (P[3] & c3);
+
+    assign SUM = P ^ {c3, c2, c1, Cin};
 endmodule
