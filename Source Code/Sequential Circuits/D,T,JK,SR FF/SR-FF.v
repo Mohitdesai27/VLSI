4bae0a420b6229327aee3f162b61656fe6eee61a
Mohitdesai27 <mohit.s.desai2705@gmail.com>
2026-05-10 10:30:19 +0530

    Create SR-FF.v

---
+++ Source Code/Sequential Circuits/D,T,JK,SR FF/SR-FF.v
@@ -0,0 +1,13 @@
+module sr_ff(
+    input S, R, clk,
+    output reg Q
+);
+always @(posedge clk) begin
+    case ({S,R})
+        2'b00: Q <= Q;
+        2'b01: Q <= 0;
+        2'b10: Q <= 1;
+        2'b11: Q <= 1'bx; // invalid
+    endcase
+end
+endmodule
